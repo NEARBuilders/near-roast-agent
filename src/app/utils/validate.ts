5dad5f10e032749e1629b25cf9c8eac908b4809f
elliotBraem <elliotBraem@users.noreply.github.com>
2024-11-25 15:51:25 +0000

    Apply formatting changes

--- src/app/utils/validate.ts
+++ src/app/utils/validate.ts
@@ -2,7 +2,10 @@ import { getAccountState, getMainnetRpcProvider } from "@near-js/client";
 
 export async function isValidNearAccount(accountId: string) {
   try {
-    await getAccountState({ account: accountId, deps: { rpcProvider: getMainnetRpcProvider() } });
+    await getAccountState({
+      account: accountId,
+      deps: { rpcProvider: getMainnetRpcProvider() },
+    });
     return true;
   } catch (e) {
     return false;
