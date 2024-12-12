683f8bff94259f719b65c6ecd6aff4f2a45e61df
Elliot Braem <elliot@ejlbraem.com>
2024-12-12 11:37:54 -0600

    adds token reputations

--- src/app/data/token-reputations.ts
+++ src/app/data/token-reputations.ts
@@ -202,5 +202,35 @@ export const TOKEN_REPUTATIONS: Record<string, ContractMetadata> = {
     risk: "medium",
     reputation: "getting paid in magic internet money to stop being so fat",
   },
+  "438e48ed4ce6beecf503d43b9dbd3c30d516e7fd.factory.bridge.near": {
+    name: "UWON",
+    category: "meme",
+    risk: "medium",
+    reputation: "Really, $UWON? Lol, lmao even. More like $ULOST"
+  },
+  "edge-fast.near": {
+    name: "FAST",
+    category: "project",
+    risk: "medium",
+    reputation: "$FAST? Really? Who do you think you are? Sonic? You need to be anything but fast when you're watching an Edge Vide (AI)"
+  },
+  "far-token.fewandfar-protocol.near": {
+    name: "FAR",
+    category: "project",
+    risk: "mega-degen",
+    reputation: "Oh, you're still holding $FAR? Looks like you took a gamble on your hopes and dreams and lost (much like the @FewandFarNFT team)"
+  },
+  "abg-966.meme-cooking.near": {
+    name: "ABG",
+    category: "meme",
+    risk: "safe",
+    reputation: "I know you think holding $ABG will help you get closer to @ekang426 but: A) she's already taken and B) you're (probably) not an extremely intelligent, borderline spectrum, man working in tech so therefore all Asian women are out of your league"
+  },
+  "token.burrow.near": {
+    name: "BRRR",
+    category: "defi",
+    risk: "safe",
+    reputation: "I can see you're holding a bag of $BRRR, appropriate because you must be freezing this winter seeing as that'll be down only"
+  }
   // what else???
 };
